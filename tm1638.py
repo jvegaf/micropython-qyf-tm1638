2078b440e6437979e6e3425d496416eb2a8ee1c1
Jose Vega <josevega234@gmail.com>
2025-02-10 20:12:25 +0100

    refactor & fixes

--- tm1638.py
+++ tm1638.py
@@ -44,10 +44,97 @@ TM1638_DSP_ON = const(8) # 0x08 display on
 TM1638_READ = const(2)   # 0x02 read key scan data
 TM1638_FIXED = const(4)  # 0x04 fixed address mode
 
-# 0-9, a-z, blank, dash, star
-_SEGMENTS = bytearray(b'\x3F\x06\x5B\x4F\x66\x6D\x7D\x07\x7F\x6F\x77\x7C\x39\x5E\x79\x71\x3D\x76\x06\x1E\x76\x38\x55\x54\x3F\x73\x67\x50\x6D\x78\x3E\x1C\x2A\x76\x6E\x5B\x00\x40\x63')
-
 class TM1638(object):
+    FONT = {
+        '0': 0b00111111,
+        '1': 0b00000110,
+        '2': 0b01011011,
+        '3': 0b01001111,
+        '4': 0b01100110,
+        '5': 0b01101101,
+        '6': 0b01111101,
+        '7': 0b00000111,
+        '8': 0b01111111,
+        '9': 0b01101111,
+        'a': 0b01110111,
+        'b': 0b01111100,
+        'c': 0b01011000,
+        'd': 0b01011110,
+        'e': 0b01111001,
+        'f': 0b01110001,
+        'g': 0b01011111,
+        'h': 0b01110100,
+        'i': 0b00010000,
+        'j': 0b00001110,
+        'l': 0b00111000,
+        'n': 0b01010100,
+        'o': 0b01011100,
+        'p': 0b01110011,
+        'r': 0b01010000,
+        's': 0b01101101,
+        't': 0b01111000,
+        'u': 0b00111110,
+        'y': 0b01101110,
+
+        # added from https://github.com/thilaire/rpi-TM1638/blob/master/rpi_TM1638/Font.py
+        ' ': 0b00000000,  # (32) <space>
+        '!': 0b10000110,  # (33) !
+        '"': 0b00100010,  # (34) "
+        '(': 0b00110000,  # (40) (
+        ')': 0b00000110,  # (41) )
+        ',': 0b00000100,  # (44) ,
+        '-': 0b01000000,  # (45) -
+        '.': 0b10000000,  # (46) .
+        '/': 0b01010010,  # (47) /
+        '=': 0b01001000,  # (61) =
+        '?': 0b01010011,  # (63) ?
+        '@': 0b01011111,  # (64) @
+        'A': 0b01110111,  # (65) A
+        'B': 0b01111111,  # (66) B
+        'C': 0b00111001,  # (67) C
+        'D': 0b00111111,  # (68) D
+        'E': 0b01111001,  # (69) E
+        'F': 0b01110001,  # (70) F
+        'G': 0b00111101,  # (71) G
+        'H': 0b01110110,  # (72) H
+        'I': 0b00000110,  # (73) I
+        'J': 0b00011111,  # (74) J
+        'K': 0b01101001,  # (75) K
+        'L': 0b00111000,  # (76) L
+        'M': 0b01010100,  # (77) M (equal to n!)
+        #'M': 0b00010101,  # (77) M
+        'N': 0b00110111,  # (78) N
+        'O': 0b00111111,  # (79) O
+        'P': 0b01110011,  # (80) P
+        'Q': 0b01100111,  # (81) Q
+        'R': 0b00110001,  # (82) R
+        'S': 0b01101101,  # (83) S
+        'T': 0b01111000,  # (84) T
+        'U': 0b00111110,  # (85) U
+        'V': 0b00101010,  # (86) V
+        'W': 0b00011101,  # (87) W
+        'X': 0b01110110,  # (88) X
+        'Y': 0b01101110,  # (89) Y
+        'Z': 0b01011011,  # (90) Z
+        '[': 0b00111001,  # (91) [
+        ']': 0b00001111,  # (93) ]
+        '_': 0b00001000,  # (95) _
+        '`': 0b00100000,  # (96) `
+        'k': 0b01110101,  # (107) k
+        'm': 0b01010101,  # (109) m
+        'q': 0b01100111,  # (113) q
+        'v': 0b00101010,  # (118) v
+        'w': 0b00011101,  # (119) w
+        'x': 0b01110110,  # (120) x
+        'z': 0b01000111,  # (122) z
+        '{': 0b01000110,  # (123) {
+        '|': 0b00000110,  # (124) |
+        '}': 0b01110000,  # (125) }
+        '~': 0b00000001,  # (126) ~
+    }
+
+
+
     """Library for the TM1638 LED display driver."""
     def __init__(self, stb, clk, dio, brightness=7):
         self.stb = stb
@@ -171,16 +258,6 @@ class TM1638(object):
             self.stb(1)
 
     def keys(self):
-        """Return a byte representing which keys are pressed. LSB is SW1"""
-        keys = 0
-        self.stb(0)
-        self._byte(TM1638_CMD1 | TM1638_READ)
-        for i in range(4):
-            keys |= self._scan_keys() << i
-        self.stb(1)
-        return keys
-
-    def qyf_keys(self):
         """Return a 16-bit value representing which keys are pressed. LSB is SW1"""
         keys = 0
         self.stb(0)
@@ -195,91 +272,70 @@ class TM1638(object):
         self.stb(1)
         return keys
 
-    def encode_digit(self, digit):
-        """Convert a character 0-9, a-f to a segment."""
-        return _SEGMENTS[digit & 0x0f]
+    def send_data(self, addr, data):
+        self._write_data_cmd()
+        self.stb(0)
+        self._byte(0xC0 | addr)
+        self._byte(data)
+        self.stb(1)
 
-    def encode_string(self, string):
-        """Convert an up to 8 character length string containing 0-9, a-z,
-        space, dash, star to an array of segments, matching the length of the
-        source string excluding dots, which are merged with previous char."""
-        segments = bytearray(len(string.replace('.','')))
-        j = 0
-        for i in range(len(string)):
-            if string[i] == '.' and j > 0:
-                segments[j-1] |= (1 << 7)
-                continue
-            segments[j] = self.encode_char(string[i])
-            j += 1
-        return segments
+    def send_char(self, pos, data, dot=False):
+        self.send_data(pos << 1, data | (128 if dot else 0))
 
-    def encode_char(self, char):
-        """Convert a character 0-9, a-z, space, dash or star to a segment."""
-        o = ord(char)
-        if o == 32:
-            return _SEGMENTS[36] # space
-        if o == 42:
-            return _SEGMENTS[38] # star/degrees
-        if o == 45:
-            return _SEGMENTS[37] # dash
-        if o >= 65 and o <= 90:
-            return _SEGMENTS[o-55] # uppercase A-Z
-        if o >= 97 and o <= 122:
-            return _SEGMENTS[o-87] # lowercase a-z
-        if o >= 48 and o <= 57:
-            return _SEGMENTS[o-48] # 0-9
-        raise ValueError("Character out of range: {:d} '{:s}'".format(o, chr(o)))
+    def set_digit(self, pos, digit, dot=False):
+        for i in range(0, 6):
+            self.send_char(i, self._bit_mask(pos, digit, i), dot)
 
-    def hex(self, val):
-        """Display a hex value 0x00000000 through 0xffffffff, right aligned, leading zeros."""
-        string = '{:08x}'.format(val & 0xffffffff)
-        self.segments(self.encode_string(string))
+    def _bit_mask(self, pos, digit, bit):
+        return ((self.FONT[digit] >> bit) & 1) << pos
 
-    def number(self, num):
-        """Display a numeric value -9999999 through 99999999, right aligned."""
-        # limit to range -9999999 to 99999999
-        num = max(-9999999, min(num, 99999999))
-        string = '{0: >8d}'.format(num)
-        self.segments(self.encode_string(string))
-
-    #def float(self, num):
-    #    # needs more work
-    #    string = '{0:>9f}'.format(num)
-    #    self.segments(self.encode_string(string[0:9]))
-
-    def temperature(self, num, pos=0):
-        """Displays 2 digit temperature followed by degrees C"""
-        if num < -9:
-            self.show('lo', pos) # low
-        elif num > 99:
-            self.show('hi', pos) # high
-        else:
-            string = '{0: >2d}'.format(num)
-            self.segments(self.encode_string(string), pos)
-        self.show('*C', pos + 2) # degrees C
-
-    def humidity(self, num, pos=4):
-        """Displays 2 digit humidity followed by RH"""
-        if num < -9:
-            self.show('lo', pos) # low
-        elif num > 99:
-            self.show('hi', pos) # high
-        else:
-            string = '{0: >2d}'.format(num)
-            self.segments(self.encode_string(string), pos)
-        self.show('rh', pos + 2) # relative humidity
-
-    def show(self, string, pos=0):
+    def show(self, text, pos=0):
         """Displays a string"""
-        segments = self.encode_string(string)
-        self.segments(segments[:8], pos)
+        dots = 0b00000000
+        dpos = text.find('.')
+        if dpos != -1:
+            # For my boards, the dot-order is non-linear:
+            # 8 4 2 1 128 64 32 16
+            realPos = dpos+(8-len(text))
+            if realPos < 0:
+              print("not possible to render: " + str(realPos) + ": " + str(dpos) + ": " + text)
+            elif realPos >= 4:
+              dots = dots | (128 >> realPos-4)
+            else:
+              dots = dots | (8 >> realPos)
+            text = text.replace('.', '')
 
-    def scroll(self, string, delay=250):
-        """Display a string, scrolling from the right to left, speed adjustable.
-        String starts off-screen right and scrolls until off-screen left."""
-        segments = string if isinstance(string, list) else self.encode_string(string)
-        data = [0] * 16
-        data[8:0] = list(segments)
-        for i in range(len(segments) + 9):
-            self.segments(data[0+i:8+i])
-            sleep_ms(delay)
+        self.send_char(7, self.rotate_bits(dots))
+        text = text[0:8]
+        text = self.rev(text)
+        text += " "*(8-len(text))
+
+        # my TM1638 board has the two 4-char displays exchanged
+        text = text[4:8] + text[0:4]
+
+        for i in range(0, 7):
+            byte = 0b00000000
+            for position in range(8):
+                c = text[position]
+                if c != ' ':
+                    byte = (byte | self._bit_mask(position, c, i))
+            self.send_char(i, self.rotate_bits(byte))
+
+    def rotate_bits(self, num):
+        for i in range(0, 4):
+            num = self.rotr(num, 8)
+        return num
+
+    def rotr(self, num, bits):
+        num &= (2**bits-1)
+        bit = num & 1
+        num >>= 1
+        if bit:
+            num |= (1 << (bits-1))
+        return num
+
+    def rev(self, s):
+        r = ""
+        for c in s:
+            r = c + r
+        return r
